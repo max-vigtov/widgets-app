01f17f72af9046bd940cd4d99a36b46cdb4c4821
Max Vigueras Tovar <mvt.2000@hotmail.com>
2023-07-22 00:31:44 -0600

    Progress Indicators Finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -29,5 +29,12 @@ const appMenuItems = <MenuItem>[
     icon: Icons.credit_card
     ),
 
+  MenuItem(
+    title: 'Progress Indicators', 
+    subTitle: 'Generales y controlados', 
+    link: '/progress', 
+    icon: Icons.refresh_rounded
+    ),    
+
   
 ];
