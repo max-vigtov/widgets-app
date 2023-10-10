113f487d2515597c194b0291c133777971a3efe6
Max Vigueras Tovar <mvt.2000@hotmail.com>
2023-10-09 22:33:42 -0600

    Infinite Scroll screen finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -63,4 +63,11 @@ const appMenuItems = <MenuItem>[
     link: '/tutorial', 
     icon: Icons.accessibility_rounded
     ),
+
+  MenuItem(
+    title: 'InfiniteScroll y Pull', 
+    subTitle: 'Listas infinitas y pull to refresh', 
+    link: '/infinite', 
+    icon: Icons.list_alt_rounded
+    ),    
 ];
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -49,6 +49,12 @@ final appRouter= GoRouter(
       path: '/tutorial',
       name: AppTutorialScreen.name,
       builder: (context, state) => const AppTutorialScreen(),
-      ),         
+      ), 
+
+    GoRoute(
+      path: '/infinite',
+      name: InfiniteScrollScreen.name,
+      builder: (context, state) => const InfiniteScrollScreen(),
+      ),               
   ]
 );
